e74c02ed255dd7b1a17ccee354b88cb127fcd12b
dougssdev <douglassabinosilva@hotmail.com>
2025-08-25 10:04:39 -0300

    create: Creating project

---
+++ src/test/java/com/br/dougssdev/apitest/ApiTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dougssdev.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
